811fc52aeedefaad5befb4dd4bebbc6f7f93d898
AliKhanMagsi <alikhan.bscsf21kdk@iba-suk.edu.pk>
2024-04-18 14:33:46 +0500

    this task is about how to concanate the lines and variable

---
+++ Day3 (concatenate).py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+food = input("Mac & cheese")
+plant = input("Cactus")
+cooking = input("Saute")
+living = input("Ruined")
+item = input("Hammer")
+
+print("Menu")
+print(cooking, food, "with", living, plant, "on a bed of", item)
+
