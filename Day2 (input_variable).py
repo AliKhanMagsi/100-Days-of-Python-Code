c3a201954d03948e0ed307dc93a47c6912d9668c
AliKhanMagsi <alikhan.bscsf21kdk@iba-suk.edu.pk>
2024-04-18 14:32:05 +0500

    this code contain input variable task

---
+++ Day2 (input_variable).py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+name = input("what's your name?: ")
+favouriteFood = input("what's your favourite food?: ")
+favouriteMusic = input("what's your favourite music?: ")
+location = input("where do you live?: ")
+
+print("you are")
+print(name)
+print("you are probabaly hungry for")
+print(favouriteFood)
+print("and you are definitely getting your groove on to")
+print(favouriteMusic)
+print("living in the amazing")
+print(location)
+
