9e920f19df83145a12e894564a80b098a2c6d21e
AliKhanMagsi <alikhan.bscsf21kdk@iba-suk.edu.pk>
2024-04-19 20:42:03 +0500

    elid command practise

---
+++ DAY-6 (elif).py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+print("Secure Login")
+username = input("What is your username?")
+password = input("What is your password?")
+
+if username == "ali" and password == "ali":
+  print("Welcome, ALi You are looking nice today!")
+
+elif username == "zain" and password == "zainzee":
+  print("Hi zain! Love your hair today!")
+
+elif username == "atif" and password == "atif":
+  print("Yo! Atif! What up?!")
+
+else:
+  print("You do not have access. Go away!")
+
+
+# In[ ]:
+
+
+
+
