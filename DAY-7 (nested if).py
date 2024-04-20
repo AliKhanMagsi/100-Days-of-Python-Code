e6a3af302592e4b92325f4359af54905e2d931ac
AliKhanMagsi <alikhan.bscsf21kdk@iba-suk.edu.pk>
2024-04-20 15:43:45 +0500

    nested if else condition practise

---
+++ DAY-7 (nested if).py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+print ("Are you a superfan of 'The Big Bang Theory' or a fake fan?")
+print()
+print("Answer these questions to find out.")
+
+Glasses = input("Does someone wear glasses?")
+if Glasses == "yes":
+  print("Correct!")
+else:
+  print("Wrong!")
+  WhoGlasses = input("And who wears glasses?")
+  if WhoGlasses == "Leonard":
+    print("You got it")
+  else:
+    print("Try again!")
+
+
+# In[ ]:
+
+
+
+
