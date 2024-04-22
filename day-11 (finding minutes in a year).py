5618c1a04ab7f217884a573fdaf76fbc7971aae8
AliKhanMagsi <alikhan.bscsf21kdk@iba-suk.edu.pk>
2024-04-22 10:41:42 +0500

    finding minutes in a year task through math operations

---
+++ day-11 (finding minutes in a year).py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+sec = 60
+min = 60
+hours = 24
+month = 12
+days = 365
+
+TotalHours = days * hours
+TotalMinutes = TotalHours * min
+totalSeconds =  TotalMinutes * sec
+print("the total seconds in a year are : ", totalSeconds)
+
+
+# In[ ]:
+
+
+
+
