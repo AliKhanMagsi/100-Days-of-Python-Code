aee26a242ed32441e426afee68d7dd8070be04fd
AliKhanMagsi <alikhan.bscsf21kdk@iba-suk.edu.pk>
2024-04-25 00:04:02 +0500

    while loop code

---
+++ day-15 (while loop).py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+num = int(input("Enter a number: "))
+while num <= 0:
+    num = int(input("That's not positive. Enter a positive number: "))
+print("You entered:", num)
+
+
+# In[ ]:
+
+
+
+
