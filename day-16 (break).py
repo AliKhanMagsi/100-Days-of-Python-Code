7717dba6cbc1f22ff65f655b14ed918e88017a32
AliKhanMagsi <alikhan.bscsf21kdk@iba-suk.edu.pk>
2024-04-25 10:55:30 +0500

    break statement in loop

---
+++ day-16 (break).py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Example 1: Using break to exit a loop when a condition is met
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for num in numbers:
+    print(num)
+    if num == 5:
+        print("Breaking the loop because num reached 5")
+        break
+
+
+# In[ ]:
+
+
+
+
