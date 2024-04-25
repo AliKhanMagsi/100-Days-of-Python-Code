3f865d54d5a78768eab54e27e509375c0fd86df9
AliKhanMagsi <alikhan.bscsf21kdk@iba-suk.edu.pk>
2024-04-25 11:01:54 +0500

    continue statement in loops

---
+++ day-17 (continue).py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[16]:
+
+
+# Filtering out odd numbers in a list
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for num in numbers:
+    if num % 2 == 0:
+        continue #skip 4
+    print(num)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
