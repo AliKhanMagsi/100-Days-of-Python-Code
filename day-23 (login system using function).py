5f83478a433958bf98776ba8a56da211f14e49d6
AliKhanMagsi <alikhan.bscsf21kdk@iba-suk.edu.pk>
2024-04-30 14:24:21 +0500

    login system using function def in python

---
+++ day-23 (login system using function).py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def login():
+  while True:
+    username = input("What is your username?: ")
+    password = input("What is your password? ")
+    if username == "David" and password == "Replit4ev#r":
+      print("Welcome David!")
+      break
+    else:
+      print("That is not the correct username or password. Try again!")
+      continue
+    
+print("REPLIT LOGIN SYSTEM")
+login()
+
